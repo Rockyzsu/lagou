85a18ea66289d99ab8f5bf2af96b4b40c5df8b15
Rockyzsu <jinweizsu@gmail.com>
2018-05-31 00:02:38 +0800

    update

--- lagou/lagou/spiders/companyspider.py
+++ lagou/lagou/spiders/companyspider.py
@@ -11,9 +11,9 @@ class CompanyInfo(scrapy.Spider):
     allowed_domains = ['lagou.com']
 
     def __init__(self):
-
+        self.citys=[213,2,3,6,184,252]
         #  获取更多城市，替换213 这个数字就可以， 根据不同城市填写
-        self.url = 'https://www.lagou.com/gongsi/215-0-0.json'
+        self.url = 'https://www.lagou.com/gongsi/{}-0-0.json'
 
         self.datas = collections.OrderedDict({"first": "false",
                                               "pn": "",
@@ -30,32 +30,24 @@ class CompanyInfo(scrapy.Spider):
                               'Host': 'www.lagou.com',
                               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
                               'Upgrade-Insecure-Requests': '1', 'Connection': 'keep-alive',
-                              'Cookie': 'user_trace_token=20160612223035-3a02d006-30aa-11e6-a343-5254005c3644; LGUID=20160612223035-3a02d566-30aa-11e6-a343-5254005c3644; JSESSIONID=ABAAABAACDBAAIA2AF78F3B190ED86CE3AD74D3F50F274F; X_HTTP_TOKEN=ae3de20baf406224ec17647191d91609; _putrc=DFAF58A586D854B0; login=true; unick=%E9%99%88%E9%94%A6%E4%BC%9F; showExpriedIndex=1; showExpriedCompanyHome=1; showExpriedMyPublish=1; hasDeliver=138; TG-TRACK-CODE=index_search; SEARCH_ID=08c78425fe834aad93ccda9367e90b39; index_location_city=%E6%B7%B1%E5%9C%B3; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1507809365,1507978370,1508072067,1509002344; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1509022033; _gat=1; LGSID=20171026201121-c740dde5-ba46-11e7-9626-5254005c3644; LGRID=20171026204711-c914afe5-ba4b-11e7-9626-5254005c3644; _ga=GA1.2.499625224.1465741835; _gid=GA1.2.1667060466.1509002344',
+                              'Cookie': 'user_trace_token=20171121112654-d2256ffd-ce6b-11e7-9971-5254005c3644; LGUID=20171121112654-d22572b4-ce6b-11e7-9971-5254005c3644; _ga=GA1.2.1514235968.1511234812; JSESSIONID=ABAAABAACBHABBI1A2002B1D4BE797A9348546D9AF5EF2A; _gid=GA1.2.1655607353.1527327820; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1527327829; index_location_city=%E5%85%A8%E5%9B%BD; SEARCH_ID=fa68e1ccb6904d33bf9eb657173121ab; TG-TRACK-CODE=gongsi_banner; LGSID=20180526232211-900a8947-60f8-11e8-8c26-5254005c3644; _gat=1; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1527350122; LGRID=20180526235527-35b6c072-60fd-11e8-a4c7-525400f775ce',
                               'Pragma': 'no-cache', 'Cache-Control': 'no-cache',
-                              'Referer': 'https://www.lagou.com/gongsi/j917.html',
+                              # 'Referer': 'https://www.lagou.com/gongsi/{}-0-0',
                               'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
 
-        self.cookies = {"user_trace_token": "20160612223035-3a02d006-30aa-11e6-a343-5254005c3644",
-                        "LGUID": "20160612223035-3a02d566-30aa-11e6-a343-5254005c3644",
-                        "JSESSIONID": "ABAAABAACDBAAIA2AF78F3B190ED86CE3AD74D3F50F274F",
-                        "X_HTTP_TOKEN": "ae3de20baf406224ec17647191d91609",
-                        "_putrc": "DFAF58A586D854B0",
-                        "login": "true",
-                        "unick": "%E9%99%88%E9%94%A6%E4%BC%9F",
-                        "showExpriedIndex": "1",
-                        "showExpriedCompanyHome": "1",
-                        "showExpriedMyPublish": "1",
-                        "hasDeliver": "138",
-                        "TG-TRACK-CODE": "index_search",
-                        "SEARCH_ID": "08c78425fe834aad93ccda9367e90b39",
-                        "index_location_city": "%E6%B7%B1%E5%9C%B3",
-                        "Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6": "1507809365,1507978370,1508072067,1509002344",
-                        "Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6": "1509022033",
+        self.cookies = {"user_trace_token": "20171121112654-d2256ffd-ce6b-11e7-9971-5254005c3644",
+                        "LGUID": "20171121112654-d22572b4-ce6b-11e7-9971-5254005c3644",
+                        "_ga": "GA1.2.1514235968.1511234812",
+                        "JSESSIONID": "ABAAABAACBHABBI1A2002B1D4BE797A9348546D9AF5EF2A",
+                        "_gid": "GA1.2.1655607353.1527327820",
+                        "Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6": "1527327829",
+                        "index_location_city": "%E5%85%A8%E5%9B%BD",
+                        "SEARCH_ID": "fa68e1ccb6904d33bf9eb657173121ab",
+                        "TG-TRACK-CODE": "gongsi_banner",
+                        "LGSID": "20180526232211-900a8947-60f8-11e8-8c26-5254005c3644",
                         "_gat": "1",
-                        "LGSID": "20171026201121-c740dde5-ba46-11e7-9626-5254005c3644",
-                        "LGRID": "20171026204711-c914afe5-ba4b-11e7-9626-5254005c3644",
-                        "_ga": "GA1.2.499625224.1465741835",
-                        "_gid": "GA1.2.1667060466.1509002344", }
+                        "Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6": "1527350122",
+                        "LGRID": "20180526235527-35b6c072-60fd-11e8-a4c7-525400f775ce", }
 
     def start_requests(self):
         yield scrapy.http.Request(url='https://www.lagou.com/gongsi/', headers=self.first_headers,
@@ -64,18 +56,20 @@ class CompanyInfo(scrapy.Spider):
                                   )
 
     def next_request(self, response):
-        # print response.body
-        #公司没那么多，只需填一个小于100的
-        for i in range(1, 100):
-            self.datas['pn'] = str(i)
-            meta = {'dont_redirect': True, 'handle_httpstatus_list': [302]}
-            yield scrapy.http.FormRequest(url=self.url, headers=self.first_headers, callback=self.parse_item,
-                                          formdata=self.datas,
-                                          cookies=self.cookies,
-                                          # meta={'cookiejar': response.meta['cookiejar'],'dont_redirect': True, 'handle_httpstatus_list': [302]},
-                                          # meta={'cookiejar': response.meta['cookiejar']},
-                                          # meta={'dont_merge_cookies': True},
-                                          dont_filter=True)
+
+        for city in self.citys:
+            # print response.body
+            # 公司没那么多，只需填一个小于100的
+            for i in range(1, 100):
+                self.datas['pn'] = str(i)
+                meta = {'dont_redirect': True, 'handle_httpstatus_list': [302]}
+                yield scrapy.http.FormRequest(url=self.url.format(city), headers=self.first_headers, callback=self.parse_item,
+                                              formdata=self.datas,
+                                              cookies=self.cookies,
+                                              # meta={'cookiejar': response.meta['cookiejar'],'dont_redirect': True, 'handle_httpstatus_list': [302]},
+                                              # meta={'cookiejar': response.meta['cookiejar']},
+                                              # meta={'dont_merge_cookies': True},
+                                              dont_filter=True)
 
     def parse_item(self, response):
         try:
